21ff61b03dcc28d017bfdf73de2c3af30abc2308
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-14 14:14:32 +0200

    Added tests of the Publication structure.
    Bump #11.

--- src/edeposit/amqp/harvester/tests/unittests/test_publication.py
+++ src/edeposit/amqp/harvester/tests/unittests/test_publication.py
@@ -4,17 +4,41 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import pytest
 
-
-
-# Variables ===================================================================
-
+from harvester.structures import Publication
 
 
 # Functions & objects =========================================================
+def test_publication():
+    p = Publication("title", "author", "pages", "price", "publisher")
 
+    # test properties of the Publication object
+    assert p.title == "title"
+    assert p.author == "author"
+    assert p.pages == "pages"
+    assert p.price == "price"
+    assert p.publisher == "publisher"
 
+    # test immutability of object, but mutability of properties
+    with pytest.raises(ValueError):
+        p.xax = 1
+    p.price = 1
+    assert p.price == 1
 
-# Main program ================================================================
-if __name__ == '__main__':
-    pass
+    # test properties of generated namedtuple
+    nt = p.to_namedtuple()
+    assert nt.title == "title"
+    assert nt.author == "author"
+    assert nt.pages == "pages"
+    assert nt.price == 1
+    assert nt.publisher == "publisher"
+
+    assert nt.optionals is None
+
+    # test that optionals can prevent setting themself to None if anything is
+    # set in them
+    p.optionals.ean = "xex"
+    nt = p.to_namedtuple()
+    assert nt.optionals is not None
+    nt.optionals.ean == "xex"
