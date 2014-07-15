02612c3a9bec57b51c70b554db4f60e70c5c17c3
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-15 17:58:11 +0200

    Partial implementation of the aleph filter (#8).

--- src/edeposit/amqp/harvester/filters/aleph_filter.py
+++ src/edeposit/amqp/harvester/filters/aleph_filter.py
@@ -4,7 +4,9 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from .. import settings
 
+import edeposit.amqp.aleph as aleph
 
 
 # Variables ===================================================================
@@ -12,9 +14,21 @@
 
 
 # Functions & objects =========================================================
+def filter(publication):
+    query = None
+    isbn_query = False
+    if publication.optionals and publication.optionals.ISBN:
+        query = aleph.ISBNQuery(publication.optionals.ISBN)
+        isbn_query = True
+    else:
+        query = aleph.TitleQuery(publication.title)
 
+    result = aleph.reactToAMQPMessage(aleph.SearchRequest(query, ""))
 
+    if not result.records:
+        return publication
 
-# Main program ================================================================
-if __name__ == '__main__':
-    pass
+    # TODO: filter by author name
+    if not isbn_query:
+        pass
+
