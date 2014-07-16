fa09530ccd23755eed396f4359862254018e307b
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-16 13:02:00 +0200

    Added name_to_vector() and compare_names().
    Bump #8.

--- src/edeposit/amqp/harvester/filters/aleph_filter.py
+++ src/edeposit/amqp/harvester/filters/aleph_filter.py
@@ -4,9 +4,11 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-from .. import settings
+import unicodedata
 
 import edeposit.amqp.aleph as aleph
+
+# from .. import settings
 
 
 # Variables ===================================================================
@@ -14,7 +16,63 @@ import edeposit.amqp.aleph as aleph
 
 
 # Functions & objects =========================================================
-def filter(publication):
+def name_to_vector(name):
+    """
+    Convert `name` to the ASCII vector.
+
+    Example:
+        >>> name_to_vector("ing. Franta Putšálek")
+        ['putsalek', 'franta', 'ing']
+
+    Args:
+        name (str): Name which will be vectorized.
+
+    Returns:
+        list: Vector created from name.
+    """
+    if not isinstance(name, unicode):
+        name = name.decode("utf-8")
+
+    name = name.lower()
+    name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore')
+    name = "".join(filter(lambda x: x.isalpha() or x == " ", list(name)))
+
+    return sorted(name.split(), key=lambda x: len(x), reverse=True)
+
+
+def compare_names(first, second):
+    """
+    Compare two names in complicated, but more error prone way.
+
+    Algorithm is using vector comparison.
+
+    Example:
+        >>> compare_names("Franta Putšálek", "ing. Franta Putšálek")
+        100.0
+        >>> compare_names("F. Putšálek", "ing. Franta Putšálek")
+        50.0
+
+    Args:
+        first (str): Fisst name as string.
+        second (str): Second name as string.
+
+    Returns:
+        float: Percentage of the similarity.
+    """
+    first = name_to_vector(first)
+    second = name_to_vector(second)
+
+    zipped = zip(first, second)
+
+    similarity_factor = 0
+    for fitem, sitem in zipped:
+        if fitem == sitem:
+            similarity_factor += 1
+
+    return (float(similarity_factor)/len(zipped)) * 100
+
+
+def filter_publication(publication):
     query = None
     isbn_query = False
     if publication.optionals and publication.optionals.ISBN:
@@ -23,7 +81,7 @@ def filter(publication):
     else:
         query = aleph.TitleQuery(publication.title)
 
-    result = aleph.reactToAMQPMessage(aleph.SearchRequest(query, ""))
+    result = aleph.reactToAMQPMessage(aleph.SearchRequest(query), "")
 
     if not result.records:
         return publication
