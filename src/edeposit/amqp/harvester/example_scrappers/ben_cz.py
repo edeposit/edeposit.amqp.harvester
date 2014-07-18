3424d67806bf21fc86e8ab8ffabc1718da8bca6a
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-18 14:07:12 +0200

    Formatting of the ben example (#13) changed to use spaces instead of tabs.

--- src/edeposit/amqp/harvester/example_scrappers/ben_cz.py
+++ src/edeposit/amqp/harvester/example_scrappers/ben_cz.py
@@ -21,43 +21,43 @@ URL = "http://shop.ben.cz/cz/kategorie/novinky/knihy-za-posledni-tri-mesice-pouz
 
 #= Functions & objects ========================================================
 def getBooks():
-	downer = Downloader()
-	data = downer.download(URL)
+    downer = Downloader()
+    data = downer.download(URL)
 
-	dom = d.parseString(data)
+    dom = d.parseString(data)
 
-	books = []
-	for html_chunk in dom.find("div", {"class": "seznamKniha"}):
-		book = {}
+    books = []
+    for html_chunk in dom.find("div", {"class": "seznamKniha"}):
+        book = {}
 
-		# name is separated by ":" from author
-		name = html_chunk.find("a")[0].params["title"]
-		if ":" in name:
-			book["author"] = name.split(":")[0].strip()
-			book["name"]   = "".join(name.split(":")[1:]).strip()
-		else:
-			book["name"] = name
+        # name is separated by ":" from author
+        name = html_chunk.find("a")[0].params["title"]
+        if ":" in name:
+            book["author"] = name.split(":")[0].strip()
+            book["name"]   = "".join(name.split(":")[1:]).strip()
+        else:
+            book["name"] = name
 
-		book["url"]   = html_chunk.find("a")[0].params["href"]
-		book["cost"]  = html_chunk.find("p", {"class": "seznamCena"})[0]\
-		                          .find("strong")[0].getContent().split("Kč")[0]\
-		                          .strip()
+        book["url"]   = html_chunk.find("a")[0].params["href"]
+        book["cost"]  = html_chunk.find("p", {"class": "seznamCena"})[0]\
+                                  .find("strong")[0].getContent().split("Kč")[0]\
+                                  .strip()
 
-		descr = downer.download(html_chunk.find("a")[0].params["href"])
-		descr = d.parseString(descr.decode("utf-8").encode("utf-8"))  # it works :D
-		descr = descr.find("div", {"class": "detailPopis"})[0]
-		descr = str(descr).split("<ol>")[0]
-		descr = descr.split("</table>")[1] if "</table>" in descr else descr
-		book["descr"] = d.removeTags(descr).strip()
+        descr = downer.download(html_chunk.find("a")[0].params["href"])
+        descr = d.parseString(descr.decode("utf-8").encode("utf-8"))  # it works :D
+        descr = descr.find("div", {"class": "detailPopis"})[0]
+        descr = str(descr).split("<ol>")[0]
+        descr = descr.split("</table>")[1] if "</table>" in descr else descr
+        book["descr"] = d.removeTags(descr).strip()
 
-		books.append(book)
+        books.append(book)
 
-	return books
+    return books
 
 
 #= Main program ===============================================================
 if __name__ == '__main__':
-	print getBooks()[0]["descr"]
+    print getBooks()[0]["descr"]
 
 # TODO:
 # serializace do jsonu jen při spuštění
