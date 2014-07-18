c7e666549daa8017cb689ad1c48ce8197cbea7e1
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-18 14:05:38 +0200

    Exception class removed - i've decided to use standard assertion errors.

--- src/edeposit/amqp/harvester/scrappers/exceptions.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Interpreter version: python 2.7
-#
-class UnitTestException(UserWarning):
-    def __init__(self, message):
-        super(UnitTestException, self).__init__(message)
