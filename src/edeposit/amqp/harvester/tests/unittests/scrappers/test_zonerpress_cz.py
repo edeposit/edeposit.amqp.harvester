f3d2be1e336fbf43b8f5bc9c2fbfac028bcb7029
Bystroushaak <bystrousak@kitakitsune.org>
2014-08-08 15:41:34 +0200

    #16, #11: Added template of the zonerpress unittest.

---
+++ src/edeposit/amqp/harvester/tests/unittests/scrappers/test_zonerpress_cz.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pytest
+import dhtmlparser as d
+
+from harvester.scrappers import zonerpress_cz
+
+
+# Variables ===================================================================
+# with pytest.raises(AssertionError):
+
+
+# Functions & objects =========================================================
