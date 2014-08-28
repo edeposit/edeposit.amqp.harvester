16ea829295cd389e02a2f38a0482a94f593c6d56
Bystroushaak <bystrousak@kitakitsune.org>
2014-08-28 13:55:00 +0200

    #20; generator.py: Added support for match() and wfind().

--- src/edeposit/amqp/harvester/autoparser/generator.py
+++ src/edeposit/amqp/harvester/autoparser/generator.py
@@ -12,24 +12,76 @@ IND = "    "
 
 
 # Functions & objects =========================================================
-def _find_template(parameters, index, required=False, notfound_msg=None):
+def _index_idiom(el_name, index, alt=None):
+    el_index = "%s[%d]" % (el_name, index)
+
+    if index == 0:
+        cond = "%s" % el_name
+    else:
+        cond = "len(%s) - 1 >= %d" % (el_name, index)
+
+    output = IND + "# pick element from list\n"
+
+    return output + IND + "%s = %s if %s else %s\n\n" % (
+        el_name,
+        el_index,
+        cond,
+        repr(alt)
+    )
+
+
+def _required_idiom(index, notfoundmsg):
+    cond = ""
+    if index > 0:
+        cond = " or len(el) - 1 < %d" % index
+
+    output = IND + "if not el%s:\n" % cond
+    output += IND + IND + "raise UserWarning(%s)\n\n" % repr(notfoundmsg)
+
+    return output + IND + "el = el[%d]\n\n" % index
+
+
+def _find_template(parameters, index, required=False, notfoundmsg=None):
     output = IND + "el = dom.find(%s)\n\n" % repr(parameters)[1:-1]
 
     if required:
-        output += IND + "if not el:\n"
-        output += IND + IND + "raise UserWarning(%s)\n\n" % repr(notfound_msg)
+        return output + _required_idiom(index, notfoundmsg)
 
-    return output
+    return output + _index_idiom("el", index)
 
 
-def _generate_parser(name, path, required=False, notfound_msg=None):
+def _wfind_template(parameters, index, required=False, notfoundmsg=None):
+    output = IND + "el = dom.wfind(%s).childs\n\n" % repr(parameters)[1:-1]
+
+    if required:
+        return output + _required_idiom(index, notfoundmsg)
+
+    return output + _index_idiom("el", index)
+
+
+def _match_template(parameters, index, required=False, notfoundmsg=None):
+    output = IND + "el = dom.match(%s)\n\n" % repr(parameters)[1:-1]
+
+    #TODO: reduce None parameters
+
+    if required:
+        return output + _required_idiom(index, notfoundmsg)
+
+    return output + _index_idiom("el", index)
+
+
+def _generate_parser(name, path, required=False, notfoundmsg=None):
     output = "def get_%s(dom):\n" % name
 
     parser_table = {
         "find": lambda path:
-            _find_template(path.params, path.index, required, notfound_msg),
+            _find_template(path.params, path.index, required, notfoundmsg),
+        "wfind": lambda path:
+            _wfind_template(path.params, path.index, required, notfoundmsg),
+        "match": lambda path:
+            _match_template(path.params, path.index, required, notfoundmsg),
     }
-    if path.call_type not in parser_table:
+    if path.call_type not in parser_table: #TODO: chained
         return ""
 
     output += parser_table[path.call_type](path)
@@ -47,8 +99,8 @@ def generate_parsers(config, paths):
         path = path[0]  # pick path with highest priority
 
         required = config[0]["vars"][name].get("required", False)
-        notfound_msg = config[0]["vars"][name].get("notfoundmsg", "")
+        notfoundmsg = config[0]["vars"][name].get("notfoundmsg", "")
 
-        output += _generate_parser(name, path, required, notfound_msg)
+        output += _generate_parser(name, path, required, notfoundmsg)
 
     return output
