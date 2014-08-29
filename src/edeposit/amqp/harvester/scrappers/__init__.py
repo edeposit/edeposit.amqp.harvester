4ee950419440a79cc1a78e646dfb75fb4556faef
Bystroushaak <bystrousak@kitakitsune.org>
2014-08-29 11:27:44 +0200

    scrappers/__init__.py; Added self test code.

--- src/edeposit/amqp/harvester/scrappers/__init__.py
+++ src/edeposit/amqp/harvester/scrappers/__init__.py
@@ -4,16 +4,13 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-import traceback
-
 import ben_cz
+import grada_cz
+import cpress_cz
 
 
 # Functions & objects =========================================================
 def self_test_all():
-    try:
-        ben_cz.self_test()
-    except Exception, e:
-        raise UserWarning(
-            e.message + "\n---\n" + traceback.format_exc().strip()
-        )
+    ben_cz.self_test()
+    grada_cz.self_test()
+    cpress_cz.self_test()
