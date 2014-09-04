acb7e91f0781ae894d9eb0f34768887890e58eb2
Bystroushaak <bystrousak@kitakitsune.org>
2014-09-04 11:40:16 +0200

    #16; Zonerpress parser now works.

--- src/edeposit/amqp/harvester/scrappers/zonerpress_cz/__init__.py
+++ src/edeposit/amqp/harvester/scrappers/zonerpress_cz/__init__.py
@@ -93,8 +93,28 @@ def get_book_links(links):
     return book_links
 
 
+def _strip_content(el):
+    content = el.getContent().strip()
+
+    if content == "-":
+        return None
+
+    return content
+
+
 def _parse_authors(authors):
-    pass
+    link = authors.find("a")
+    link = link[0].params.get("href") if link else None
+
+    author_list = _strip_content(authors)
+
+    if "(" in author_list:
+        author_list = author_list.split("(")[0]
+
+    return map(
+        lambda author: structures.Author(author.strip(), link),
+        author_list.strip().split(",")
+    )
 
 
 def _process_book(link):
@@ -104,16 +124,29 @@ def _process_book(link):
     )
     dhtmlparser.makeDoubleLinked(dom)
 
-    authors = _parse_authors(zapi.get_author(dom))
-    authors = [structures.Author("xex")]
-
     pub = structures.Publication(
-        title=zapi.get_title(dom).getContent().strip(),
-        authors=authors,
-        price=zapi.get_price(dom).getContent().strip(),
-        publisher=zapi.get_publisher(dom).getContent().strip()
+        title=_strip_content(zapi.get_title(dom)),
+        authors=_parse_authors(zapi.get_author(dom)),
+        price=_strip_content(zapi.get_price(dom)),
+        publisher=_strip_content(zapi.get_publisher(dom))
     )
 
+    pub.optionals.URL = link
+    pub.optionals.pages = _strip_content(zapi.get_pages(dom))
+    pub.optionals.pub_date = _strip_content(zapi.get_pub_date(dom))
+    pub.optionals.ISBN = _strip_content(zapi.get_ISBN(dom))
+    pub.optionals.binding = _strip_content(zapi.get_binding(dom))
+
+    # post checks
+    if pub.title.startswith("E-kniha:"):
+        pub.title = pub.title.replace("E-kniha:", "", 1).strip()
+        pub.optionals.is_ebook = True
+
+    if " " in pub.optionals.ISBN:
+        pub.optionals.ISBN = pub.optionals.ISBN.split(" ")[0]
+
+    if "(" in pub.optionals.ISBN:
+        pub.optionals.ISBN = pub.optionals.ISBN.split("(")[0]
 
     return pub
 
